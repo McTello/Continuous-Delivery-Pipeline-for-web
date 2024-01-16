00d098d8dd66f3d562586766e555afa9eef7100f
Diogo.Mic <okolimichael2015@outlook.com>
2024-01-16 13:44:59 +0100

    Full Pipeline Test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Michael Diogo the Cloud Engineer with DevOps practice'));
+app.get('/', (req, res) => res.send('Michael Diogo the Cloud Engineer with DevOps Expertise'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
